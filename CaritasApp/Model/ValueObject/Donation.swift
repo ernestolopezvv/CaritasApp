50080290aadcbbd14f8f26999725567376693944
Enrique Ruiz <jera_24@outlook.com>
2021-09-29 15:23:55 -0600

    Firebase y Analítica

---
+++ CaritasApp/Model/ValueObject/Donation.swift
@@ -0,0 +1,12 @@
+//
+//  Donation.swift
+//  CaritasApp
+//
+//  Created by Enrique Ruiz on 29/09/21.
+//
+
+import Foundation
+
+public class Donation: Codable {
+    
+}
---
+++ CaritasApp/Model/ValueObject/Item.swift
@@ -0,0 +1,12 @@
+//
+//  Item.swift
+//  CaritasApp
+//
+//  Created by Enrique Ruiz on 29/09/21.
+//
+
+import Foundation
+
+public class Item: Codable {
+    
+}
