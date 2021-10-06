770cc40c17c4adb7c26b117373a109da120a51d7
Santiago Arzaga <73621292+SantiagoArzaga@users.noreply.github.com>
2021-10-06 00:55:04 -0600

    Calendario
    Se agrego la dependencia FSCalendar, una clase IDS14 para poder agregar funcionalidad al calendario y un View Controller con un UIView de clase FSCalendar

---
+++ CaritasApp/IDS14.swift
@@ -0,0 +1,26 @@
+//
+//  IDS14.swift
+//  CaritasApp
+//
+//  Created by Santiago A.A.M on 06/10/21.
+//
+
+import UIKit
+import FSCalendar
+
+class IDS14: UIViewController, FSCalendarDelegate {
+    
+    @IBOutlet var calendar: FSCalendar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calendar.delegate = self
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE MM-dd-YYYY"
+        let string = formatter.string(from: date)
+        print("\(string)")
+    }
+}
