b634ce0f6149aabd80a9a30d150e55b299208f81
Ernesto López <52114915+ernestolopezvv@users.noreply.github.com>
2021-10-22 18:52:40 -0600

    Se removio contrasena del schema de usuario

--- CaritasApp/Model/ValueObject/User.swift
+++ CaritasApp/Model/ValueObject/User.swift
@@ -10,7 +10,6 @@ import Foundation
 struct User: Decodable {
     var _id: String
     var apellidos: String
-    var contrasena: String?
     var correo: String
     var entidad: String
     var nombres: String
